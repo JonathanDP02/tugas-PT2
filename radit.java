6b5d35f567cf249b0e5f6d871795f7b3ce65c0f1
JonathanDP02 <jonatanpangestu23@gmail.com>
2024-09-05 16:37:49 +0700

    Delete radit.java

--- radit.java
+++
@@ -1,7 +0,0 @@
-public class radit {
-    public static void main(String[] args) {
-        System.out.println("Nama\t: Radit firansah");
-        System.out.println("NIM\t: 244012070196");
-
-    }
-}
