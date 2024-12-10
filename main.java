a885b65d84920ca17c0d374032e81299592f7415
Raditfiransah <raditfiransah180@gmail.com>
2024-12-10 09:55:58 +0700

    123

--- main.java
+++ main.java
@@ -1,8 +1,9 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Nama : Radit Firansah");
-        System.out.println("NIM :244107020196 ");
+        String nama = "Radit"
+        System.out.println(nama);
+
         
     }
 }
